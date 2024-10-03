56e0aa7cafb03f7a9f0e344a846b553ff2e738e3
3vdi <unitcomics.world@proton.me>
2024-10-03 17:03:11 +0530

    3rd

--- prinsium/tailwind.config.mjs
+++ prinsium/tailwind.config.mjs
@@ -1,31 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			animation: {
-				marqueeOne: "marqueeOne 25s linear infinite",
-				marqueeTwo: "marqueeTwo 25s linear infinite",
-			  },
-			  keyframes: {
-				marqueeOne: {
-				  from: {
-					transform: 'translateX(0)',
-				  },
-				  to: {
-					transform: 'translateX(calc(-100% - 2.5rem))',
-				  },
-				},
-				marqueeTwo: {
-					from: {
-					  transform: 'translateX(calc(-100% - 2.5rem))',
-					},
-					to: {
-					  transform: 'translateX(0)',
-					},
-				  },
-			  },
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        shrinkToNormal: "shrinkToNormal 1s ease-in-out forwards",
+        marqueeOne: "marqueeOne 25s linear infinite",
+        marqueeTwo: "marqueeTwo 25s linear infinite",
+      },
+      keyframes: {
+        shrinkToNormal: {
+          "0%": {
+            transform: "scale(1)",
+            opacity: ".3",
+            border: "2px solid red",
+            // backgroundColor: "transparent",
+          },
+          "100%": {
+            transform: "scale(0.5)",
+            opacity: "1",
+            border: "2px solid transparent",
+            // backgroundColor: "red",
+          },
+        },
+        marqueeOne: {
+          from: {
+            transform: "translateX(0)",
+          },
+          to: {
+            transform: "translateX(calc(-100% - 2.5rem))",
+          },
+        },
+        marqueeTwo: {
+          from: {
+            transform: "translateX(calc(-100% - 2.5rem))",
+          },
+          to: {
+            transform: "translateX(0)",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
