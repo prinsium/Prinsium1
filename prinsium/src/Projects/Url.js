b1baaa2b334793af883b98c0387d5e2ef994f544
3vdi <unitcomics.world@proton.me>
2024-10-12 14:54:34 +0530

    9th

---
+++ prinsium/src/Projects/Url.js
@@ -0,0 +1,9 @@
+export const urlData = {
+instagram : 'https://www.instagram.com/prinsium/',
+twitter : 'https://www.x.com/prinsium/',
+youtube : 'https://www.youtube.com/@prinsium/',
+github : 'https://github.com/prinsium',
+linkedin : 'https://www.instagram.com/prinsium/',
+contact : 'helloprinsium@gmail.com',
+fiverr : 'https://www.instagram.com/prinsium/',
+}
--- prinsium/src/Projects/Work.js
+++ prinsium/src/Projects/Work.js
@@ -1,19 +1,19 @@
 export const workData = [
     {
         id: 1,
-        url: 'https://cdn.prod.website-files.com/63c560228fad5afd4ddbe1b3/65562e7d2f7dcf664aabaa4c_cover%20(1).webp',
+        url: "/work/astroarun.png",
     },
     {
         id: 2,
-        url: 'https://cdn.prod.website-files.com/63c560228fad5afd4ddbe1b3/65562e7d2f7dcf664aabaa4c_cover%20(1).webp',
+        url: "/work/rachnakaar.png",
     },
     {
         id: 3,
-        url: 'https://cdn.prod.website-files.com/63c560228fad5afd4ddbe1b3/65562e7d2f7dcf664aabaa4c_cover%20(1).webp',
+        url: "/work/des.png",
     },
     {
         id: 4,
-        url: 'https://cdn.prod.website-files.com/63c560228fad5afd4ddbe1b3/65562e7d2f7dcf664aabaa4c_cover%20(1).webp',
+        url: "/work/aksh.png",
     },
 
 ]
