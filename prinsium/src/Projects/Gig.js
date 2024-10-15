f68fbd9b2a0a63d32b295a4b9e6ab72e2f9c8123
3vdi <unitcomics.world@proton.me>
2024-10-15 17:58:09 +0530

    15th

--- prinsium/src/Projects/Gig.js
+++ prinsium/src/Projects/Gig.js
@@ -2,26 +2,31 @@ export const gigData = [
     {
         id: 1,
         gig: 'Figma To Design',
+        url: 'https://cdn.prod.website-files.com/63c560228fad5afd4ddbe1b3/65562e7d2f7dcf664aabaa4c_cover%20(1).webp',
         gigImage: 'https://cdn.prod.website-files.com/63c560228fad5afd4ddbe1b3/65562e7d2f7dcf664aabaa4c_cover%20(1).webp',
     },
     {
         id: 2,
         gig: 'Design To Code',
+        url: 'https://cdn.prod.website-files.com/63c560228fad5afd4ddbe1b3/65562e7d2f7dcf664aabaa4c_cover%20(1).webp',
         gigImage: 'https://cdn.prod.website-files.com/63c560228fad5afd4ddbe1b3/65562e7d2f7dcf664aabaa4c_cover%20(1).webp',
     },
     {
         id: 3,
         gig: 'Landing Page',
+        url: 'https://cdn.prod.website-files.com/63c560228fad5afd4ddbe1b3/65562e7d2f7dcf664aabaa4c_cover%20(1).webp',
         gigImage: 'https://cdn.prod.website-files.com/63c560228fad5afd4ddbe1b3/65562e7d2f7dcf664aabaa4c_cover%20(1).webp',
     },
     {
         id: 4,
         gig: 'Web Design',
+        url: 'https://cdn.prod.website-files.com/63c560228fad5afd4ddbe1b3/65562e7d2f7dcf664aabaa4c_cover%20(1).webp',
         gigImage: 'https://cdn.prod.website-files.com/63c560228fad5afd4ddbe1b3/65562e7d2f7dcf664aabaa4c_cover%20(1).webp',
     },
     {
         id: 5,
         gig: 'Front End Development',
+        url: 'https://cdn.prod.website-files.com/63c560228fad5afd4ddbe1b3/65562e7d2f7dcf664aabaa4c_cover%20(1).webp',
         gigImage: 'https://cdn.prod.website-files.com/63c560228fad5afd4ddbe1b3/65562e7d2f7dcf664aabaa4c_cover%20(1).webp',
     },
 ]
