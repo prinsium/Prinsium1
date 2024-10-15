f64bee7b8a975790ef0d0e9551c5324c131e966e
3vdi <unitcomics.world@proton.me>
2024-10-15 19:01:43 +0530

    19

--- prinsium/astro.config.mjs
+++ prinsium/astro.config.mjs
@@ -11,13 +11,22 @@
 //   adapter: vercel()
 // });
 
+// import { defineConfig } from 'astro/config';
+// import tailwind from '@astrojs/tailwind';
+// import vercelServerless from '@astrojs/vercel/serverless';
+
+// https://astro.build/config
+// export default defineConfig({
+//   integrations: [tailwind()],
+//   output: 'server',
+//   adapter: vercelServerless(),
+// });
+
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import vercelServerless from '@astrojs/vercel/serverless';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server',
-  adapter: vercelServerless(),
+  integrations: [tailwind()]
 });
