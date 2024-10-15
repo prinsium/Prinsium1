0517ccb815c280613575e9d7cbaf60fe94f2b187
3vdi <unitcomics.world@proton.me>
2024-10-15 18:30:18 +0530

    17th

--- prinsium/astro.config.mjs
+++ prinsium/astro.config.mjs
@@ -1,9 +1,23 @@
+// import { defineConfig } from 'astro/config';
+
+// import tailwind from '@astrojs/tailwind';
+
+// import vercel from '@astrojs/vercel';
+
+// // https://astro.build/config
+// export default defineConfig({
+//   integrations: [tailwind()],
+//   output: 'server',
+//   adapter: vercel()
+// });
+
 import { defineConfig } from 'astro/config';
-// Import /static for a static site
-import vercelStatic from '@astrojs/vercel/static';
- 
+import tailwind from '@astrojs/tailwind';
+import vercelServerless from '@astrojs/vercel/serverless';
+
+// https://astro.build/config
 export default defineConfig({
-  // Must be 'static' or 'hybrid' 
-  output: 'static',
-  adapter: vercelStatic(),
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: vercelServerless(),
 });
